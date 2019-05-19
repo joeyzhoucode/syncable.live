edcd89d7fee53fa91489559c8718ba50d4ea8481
Yi Zhou <yzhou.code@gmail.com>
2019-05-19 11:40:15 -0400

    Welcome static page

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -2,6 +2,6 @@ require 'rails/application_controller'
 
 class StaticController < Rails::ApplicationController
   def index
-    render file: Rails.root.join('public', 'index.html')
+    render file: Rails.root.join('public', 'welcome.html')
   end
 end
