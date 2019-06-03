2902206d78657c1a5763aff9a5a2976d4ee305ce
Yi Zhou <yzhou.code@gmail.com>
2019-06-02 22:04:37 -0400

    A bit more secure

--- client/src/index.js
+++ client/src/index.js
@@ -13,13 +13,14 @@ import "./assets/css/syncable-react.css?v=0.1.0";
 
 if(window.location.pathname === '/') {
   window.location.href='/welcome';
+} else {
+  const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+  const hist = createBrowserHistory();
+  const store = createStore(
+    rootReducer(hist),
+    composeEnhancer(applyMiddleware(thunk), applyMiddleware(routerMiddleware(hist)))
+  );
+
+  render(<Root store={store} history={hist} />, document.getElementById("root"));
 }
 
-const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const hist = createBrowserHistory();
-const store = createStore(
-  rootReducer(hist),
-  composeEnhancer(applyMiddleware(thunk), applyMiddleware(routerMiddleware(hist)))
-);
-
-render(<Root store={store} history={hist} />, document.getElementById("root"));
